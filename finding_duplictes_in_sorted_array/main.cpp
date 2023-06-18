03c169524ac22705911860d64f2bb41402563947
ubyhzargam <ubyhzargam@gmail.com>
2023-06-18 17:01:28 +0530

    Update main.cpp

--- finding_duplictes_in_sorted_array/main.cpp
+++ finding_duplictes_in_sorted_array/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int arr[20];
+    int lastdup=0,n,i,c=0;
+    cout<<"Enter the number of elements in the array : "<<endl;
+    cin>>n;
+    cout<<"Enter the elements in the array : "<<endl;
+    for(i=0;i<n;i++)
+        cin>>arr[i];
+    cout<<"The duplicates in the entered array are given by : "<<endl;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==arr[i+1])
+        {
+            lastdup=arr[i];
+            c++;
+            continue;
+        }
+        if(lastdup!=arr[i+1]&&lastdup==arr[i])
+        {
+            cout<<"The element "<<arr[i]<<" was repeated "<<c<<" times "<<endl;
+            c=0;
+        }
+    }
     return 0;
 }
